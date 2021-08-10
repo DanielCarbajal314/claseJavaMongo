3813572a360626b03c67d798be2ea98764ed45fe
dcarbajal <daniel.carbajal@pucp.edu.pe>
2021-08-09 19:55:24 -0500

    Doing queries

--- demoJava/src/main/java/com/example/demo/DemoApplication.java
+++ demoJava/src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -20,18 +23,32 @@ public class DemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(PersonRepository personRepository, CourseRepository courseRepository) {
+	public CommandLineRunner demo(PersonRepository personRepository, CourseRepository courseRepository, MongoTemplate mongoTemplate) {
 		return (args) -> {
-			personRepository.save(new Person("Daniel","Carbajal",31));
-			System.out.println("Una persona ha sido registarda");
-			var course = new Course();
-			course.name = "Lenguaje De Programacion 2";
-			course.teachersName = "Claudia Zapata";
-			course.StudentIds = new ArrayList<>();
-			course.StudentIds.add("6111bd3bf43eda7051c9d75d");
-			course.StudentIds.add("6111c069ec1798610deb860e");
-			course.StudentIds.add("6111c1650f6adf5d271fd27b");
-			courseRepository.save(course);
+			// personRepository.save(new Person("Luis","Carbajal",31));
+			// System.out.println("Una persona ha sido registarda");
+			// var course = new Course();
+			// course.name = "Lenguaje De Programacion 2";
+			// course.teachersName = "Claudia Zapata";
+			// course.StudentIds = new ArrayList<>();
+			// course.StudentIds.add("6111bd3bf43eda7051c9d75d");
+			// course.StudentIds.add("6111c069ec1798610deb860e");
+			// course.StudentIds.add("6111c1650f6adf5d271fd27b");
+			// courseRepository.save(course);
+			// var people = personRepository.findByFirstName("Daniel");
+			
+
+			var query = new Query();
+			query.addCriteria(
+				new Criteria("age").gte(20).lt(100)
+			)
+			.fields()
+				.include("firstName")
+				.include("lastName")
+				.exclude("id");
+			var people = mongoTemplate.find(query, Person.class);
+			people.stream().forEach(person -> System.out.println(person));
+
 		};
 	}
 }
--- demoJava/src/main/java/com/example/demo/repository/PersonRepository.java
+++ demoJava/src/main/java/com/example/demo/repository/PersonRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import com.example.demo.models.Person;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -8,5 +10,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonRepository extends MongoRepository<Person, String> {
-    
+    public List<Person> findByFirstName(String firstName);
 }
