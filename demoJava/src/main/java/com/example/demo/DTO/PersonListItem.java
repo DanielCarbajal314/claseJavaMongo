18a3ae95c1e76a3141c595af09488717fb156882
dcarbajal <daniel.carbajal@pucp.edu.pe>
2021-08-09 20:40:46 -0500

    My first agregation over SPRING !!

---
+++ demoJava/src/main/java/com/example/demo/DTO/PersonListItem.java
@@ -0,0 +1,8 @@
+package com.example.demo.DTO;
+
+public class PersonListItem {
+    public String personName;
+    public String personId;
+
+    public PersonListItem() {}
+}
--- demoJava/src/main/java/com/example/demo/DemoApplication.java
+++ demoJava/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.util.ArrayList;
 
+import com.example.demo.DTO.PersonListItem;
 import com.example.demo.models.Course;
 import com.example.demo.models.Person;
 import com.example.demo.repository.CourseRepository;
@@ -12,6 +13,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
@@ -38,17 +42,33 @@ public class DemoApplication {
 			// var people = personRepository.findByFirstName("Daniel");
 			
 
-			var query = new Query();
-			query.addCriteria(
-				new Criteria("age").gte(20).lt(100)
-				.and("firstName").is("Luis")
-			)
-			.fields()
-				.include("firstName")
-				.include("lastName")
-				.exclude("id");
-			var people = mongoTemplate.find(query, Person.class);
-			people.stream().forEach(person -> System.out.println(person));
+			// var query = new Query();
+			// query.addCriteria(
+			// 	new Criteria("age").gte(20).lt(100)
+			// 	.and("firstName").is("Luis")
+			// )
+			// .fields()
+			// 	.include("firstName")
+			// 	.include("lastName")
+			// 	.exclude("id");
+			// var people = mongoTemplate.find(query, Person.class);
+			// people.stream().forEach(person -> System.out.println(person));
+
+			//EJEMPLO DE AGREGACION
+			var matchCriteria = new Criteria("firstName").is("Luis");
+			MatchOperation match = Aggregation.match(matchCriteria);			
+			ProjectionOperation projection = Aggregation.project()
+				.and("firstName").as("personName")
+				.and("_id").as("personId");
+			var aggregation = Aggregation.newAggregation(match,projection);
+			var result = mongoTemplate.aggregate(aggregation, "person", PersonListItem.class);
+			var people = result.getMappedResults();
+			people.stream().forEach(person -> {
+				System.out.println(person.personId);
+				System.out.println(person.personName);
+			});
+
+
 
 		};
 	}
